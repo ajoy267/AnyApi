fbea6733d18fbe991114913413bc2b7832dfbd2b
ajoy267 <andrew.joy12@hotmail.com>
2022-03-15 17:23:02 -0700

    wrote my test and then made my model and controller for my getting the list of cats"

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('anyapi routes', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('anyapi routes', () => {
     };
     const res = await request(app).post('/api/v1/cats').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of cats', async () => {
+    const expected = await Cat.findAll();
+    const res = await request(app).get('/api/v1/cats');
+    expect(res.body).toEqual(expected);
   });
 });
